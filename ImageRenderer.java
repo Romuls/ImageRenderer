d2119eb3bffb42f50890e2428d73626e97f8e33c
Romuls <liamhidet@gmail.com>
2018-05-01 21:18:52 +1200

    Changed initial animate method and started animated image method

--- ImageRenderer.java
+++ ImageRenderer.java
@@ -20,83 +20,22 @@ import java.awt.Color;
 public class ImageRenderer{
     public static final int TOP = 20;   // top edge of the image
     public static final int LEFT = 20;  // left edge of the image
-    public static final int PIXEL_SIZE = 2;  
-
+    public static final int PIXEL_SIZE = 2;
     /** Core:
      * Renders a ppm image file.
      * Asks for the name of the file, then calls renderImageHelper.
      */
     public void renderP3Image(){
-        int pixelX = LEFT;
-        int pixelY = TOP;
-        int pixelCountX = 0;
-        int pixelCountY = 0;
-
         try {
             File imageFile = new File(UIFileChooser.open());
             Scanner imageScan = new Scanner(imageFile);
-            String first = imageScan.next();
-
-
-            if(first.equals("P3")) {
-                imageScan.nextLine();
-                int imageX = imageScan.nextInt();
-                int imageY = imageScan.nextInt();
-
-                imageScan.nextLine();
-
-                int colourVal = imageScan.nextInt();
-
-              while(imageScan.hasNextLine()) {
-                 String line = imageScan.nextLine();
-                 Scanner lineScan = new Scanner(line);
-
-                    //UI.println(line);
-                 while (lineScan.hasNext()) {
-
-
-                     int r = lineScan.nextInt();
-                     int g = lineScan.nextInt();
-                     int b = lineScan.nextInt();
-                     //UI.println(r+" "+g+" "+b);
-
-
-                    Color pixelColor = new Color(r,g,b);
-                    UI.setColor(pixelColor);
-
-                    if(pixelCountX != (imageX-1)) {
-                        UI.fillRect(pixelX,pixelY,PIXEL_SIZE,PIXEL_SIZE);
-                        //UI.println("drew pixel of values "+r+" "+g+" "+b+" @ "+pixelX+" "+pixelY);
-                       // UI.println(pixelCountX);
-                        //UI.println(imageX);
-
-                        pixelCountX++;
-                        pixelX = pixelX + PIXEL_SIZE;
-
-                    }else{
-                        pixelX = LEFT;
-                        pixelCountX = 0;
-                        pixelY = pixelY + PIXEL_SIZE;
-
-                    }
-
-
-                 }
-
-             }}else{
-                UI.println("Invalid File");
-
-
-
-
-
-           }
+            renderImageHelper(imageScan);
 
 
 
         }catch(IOException e) {UI.printf("File Failure %s \n", e);}
-
     }
+
 
     /** Core:
      * Renders a ppm image file.
@@ -110,7 +49,71 @@ public class ImageRenderer{
      * The remaining tokens are the pixel values (red, green, blue for each pixel)
      */
     public void renderImageHelper(Scanner sc){
-        /*# YOUR CODE HERE */
+        int pixelX = LEFT;
+        int pixelY = TOP;
+        int pixelCountX = 0;
+        int pixelCountY = 0;
+
+           // Scanner imageScan = new Scanner(imageFile);
+            String first = sc.next();
+
+
+            if(first.equals("P3")) {
+                sc.nextLine();
+                int imageX = sc.nextInt();
+                int imageY = sc.nextInt();
+
+                sc.nextLine();
+
+                int colourVal = sc.nextInt();
+
+                while(sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    Scanner lineScan = new Scanner(line);
+
+                    //UI.println(line);
+                    while (lineScan.hasNext()) {
+
+
+                        int r = lineScan.nextInt();
+                        int g = lineScan.nextInt();
+                        int b = lineScan.nextInt();
+                        //UI.println(r+" "+g+" "+b);
+
+
+                        Color pixelColor = new Color(r,g,b);
+                        UI.setColor(pixelColor);
+                        if(pixelCountX != (imageX-1)) {
+                            UI.fillRect(pixelX,pixelY,PIXEL_SIZE,PIXEL_SIZE);
+                            //UI.println("drew pixel of values "+r+" "+g+" "+b+" @ "+pixelX+" "+pixelY);
+                            // UI.println(pixelCountX);
+                            //UI.println(imageX);
+
+                            pixelCountX++;
+                            pixelX = pixelX + PIXEL_SIZE;
+
+                        }else{
+                            pixelX = LEFT;
+                            pixelCountX = 0;
+                            pixelY = pixelY + PIXEL_SIZE;
+
+                        }
+
+
+                    }
+
+                }}else{
+                UI.println("Invalid File");
+
+
+
+
+
+            }
+
+
+
+
 
     }
 
@@ -122,9 +125,81 @@ public class ImageRenderer{
      * Repeats the sequence 3 times.
      */
     public void renderAnimatedImage(){
-        /*# YOUR CODE HERE */
+        try {
+            File imageFile = new File(UIFileChooser.open());
+            Scanner sc = new Scanner(imageFile);
 
+
+        int pixelX = LEFT;
+        int pixelY = TOP;
+        int pixelCountX = 0;
+        int pixelCountY = 0;
+
+        // Scanner imageScan = new Scanner(imageFile);
+        String first = sc.next();
+
+
+        if(first.equals("P3")) {
+            sc.nextLine();
+            int imageX = sc.nextInt();
+            int imageY = sc.nextInt();
+
+            sc.nextLine();
+
+            int colourVal = sc.nextInt();
+
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                Scanner lineScan = new Scanner(line);
+
+                //UI.println(line);
+                while (lineScan.hasNext()) {
+
+
+                    int r = lineScan.nextInt();
+                    int g = lineScan.nextInt();
+                    int b = lineScan.nextInt();
+                    //UI.println(r+" "+g+" "+b);
+
+
+                    Color pixelColor = new Color(r,g,b);
+                    UI.setColor(pixelColor);
+                    if(pixelCountX != (imageX-1)) {
+                        UI.fillRect(pixelX,pixelY,PIXEL_SIZE,PIXEL_SIZE);
+                        //UI.println("drew pixel of values "+r+" "+g+" "+b+" @ "+pixelX+" "+pixelY);
+                        // UI.println(pixelCountX);
+                        //UI.println(imageX);
+
+                        pixelCountX++;
+                        pixelX = pixelX + PIXEL_SIZE;
+
+                    }else{
+                        pixelX = LEFT;
+                        pixelCountX = 0;
+                        pixelY = pixelY + PIXEL_SIZE;
+
+                    }
+
+
+                }
+
+            }}else{
+            UI.println("Invalid File");
+
+
+
+
+
+        }
+
+
+
+        }catch(IOException e) {UI.printf("File Failure %s \n", e);}
     }
+
+
+
+
 
     /** Completion
      * Renders a ppm image file which may be animated (multiple images in the file)
